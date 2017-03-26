c28208baad1d3ae70e32d2231c96fec04984ad6b
Andriy Yahello <andrey.lv.west@gmail.com>
2017-03-26 17:49:51 +0300

    message

---
+++ Credit/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 700;
+
+            Console.WriteLine("Please, type amount of money you want to pay. 700 is needed");
+
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Status(a, b);
+
+            Console.ReadKey();
+        }
+
+        static void Status(int a, int c)
+        {
+
+            if (c>a){
+                Console.WriteLine("You have overpayed {0}", c - a); 
+            }else if(c<a) {
+                 Console.WriteLine("{0} left to pay", a - c);
+            }
+            else
+            {
+                Console.WriteLine("loan repaid");
+            }
+        }
+    }
+}
